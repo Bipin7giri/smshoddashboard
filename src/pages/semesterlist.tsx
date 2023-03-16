5cb52221e4e4a76f774aac74fe37f9ddff2a9b3a
bipin7giri <bipingiri27@gmail.com>
2023-03-16 22:03:46 +0545

    added subject

--- src/pages/semesterlist.tsx
+++ src/pages/semesterlist.tsx
@@ -1,4 +1,4 @@
-import { mdiEye, mdiTrashCan } from '@mdi/js'
+import { mdiEye, mdiPlus, mdiTrashCan } from '@mdi/js'
 import React, { ReactElement, useEffect, useState } from 'react'
 import { useDepartments, useSampleClients, useSemester } from '../hooks/sampleData'
 import { Client, Department, Semester } from '../interfaces'
@@ -14,14 +14,64 @@ import LayoutAuthenticated from '../layouts/Authenticated'
 import CardBox from '../components/CardBox'
 import SectionMain from '../components/SectionMain'
 const { confirm } = Modal
-import { Menu, Dropdown } from 'antd'
+import { Menu, Dropdown, Select } from 'antd'
 import { useRouter } from 'next/router'
 import Swal from 'sweetalert2'
 const key = 'updatable'
 import { EditOutlined } from '@ant-design/icons'
+import { GetAccessToken } from '../helper/getAccessToken'
 const SemesterList = () => {
   const { searchData } = useAppSelector((state) => state.search)
   const { semesters, isLoading, isError, mutate } = useSemester()
+  const [users, setUsers] = useState([])
+  const [subjectName, setSubjectName] = useState('')
+  const [teacher, setTeacher] = useState('')
+  const [semesterId, setSemesterId] = useState()
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const fetchRoles = async () => {
+    const res: any = await api('/hod/department/student', {
+      headers: {
+        Authorization: GetAccessToken() || null,
+      },
+    })
+    const data = res?.data.userId
+    console.log(data)
+    const tempRoles = []
+    data?.map((user) => {
+      tempRoles.push({ value: user.id, label: user.email })
+    })
+    setUsers([...tempRoles])
+  }
+  useEffect(() => {
+    fetchRoles()
+  }, [])
+  const showModal = (id: any) => {
+    setSemesterId(id)
+    setIsModalOpen(true)
+  }
+
+  const handleOk = () => {
+    api
+      .post('hod/subject', {
+        subjectName: subjectName,
+        teacherId: teacher,
+        semesterId: semesterId,
+      })
+      .then((res) => {
+        openNotification('successfully added')
+        mutate()
+      })
+      .catch((err) => {
+        openNotification(err.message)
+      })
+    setIsModalOpen(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  }
+
   const router = useRouter()
   const openNotification = (message: string) => {
     notification.open({
@@ -36,9 +86,7 @@ const SemesterList = () => {
       inputLabel: 'Semester Name',
       inputPlaceholder: 'Semester Name',
     })
-    const handleAddSubject = async ()=>{
-
-    }
+    const handleAddSubject = async () => {}
 
     if (data) {
       console.log(data)
@@ -46,6 +94,7 @@ const SemesterList = () => {
         .patch(`/hod/semester/${id}`, { name: data })
         .then((res: any) => {
           Swal.fire(`${res.data.message}`)
+          mutate(null)
         })
         .catch((err) => {
           Swal.fire(`${err}`)
@@ -74,6 +123,26 @@ const SemesterList = () => {
     })
   }
 
+  const addSubjects = (id: any) => {
+    confirm({
+      content: 'Do you want to block this user?',
+      onOk() {
+        // api
+        //   .delete(`/hod/semester/${id}`)
+        //   .then((res) => {
+        //     openNotification(res.data.message)
+        //     mutate(null)
+        //   })
+        //   .catch((err) => {
+        //     openNotification(err.message)
+        //   })
+      },
+      onCancel() {
+        console.log('Cancel')
+      },
+      okButtonProps: { style: { backgroundColor: 'red', borderColor: 'red' } },
+    })
+  }
   const perPage = 10
 
   const [currentPage, setCurrentPage] = useState(0)
@@ -110,22 +179,46 @@ const SemesterList = () => {
     setSemester([...tempSemester])
   }, [])
 
-  const changeRole = async (roleId: string, userId: any) => {
-    // api
-    //   .patch('auth/users-roles', {
-    //     roleId: roleId,
-    //     userId: userId,
-    //   })
-    //   .then((res) => {
-    //     openNotification(res?.data?.message)
-    //     mutate(null)
-    //   })
-    //   .catch((err) => {
-    //     alert(err)
-    //   })
-  }
   return (
     <>
+      <Modal
+        okButtonProps={{
+          style: { backgroundColor: 'blue' },
+        }}
+        title="Add Subject"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <div>
+          <input
+            id="subjectname"
+            value={subjectName}
+            type="text"
+            className=" my-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Subject Name"
+            onChange={(e: any) => {
+              setSubjectName(e.target.value)
+            }}
+          />
+          <Select
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            showSearch
+            placeholder="Search For Teacher"
+            optionFilterProp="children"
+            filterOption={(input, option) => (option?.label ?? '').includes(input)}
+            filterSort={(optionA, optionB) =>
+              (optionA?.label ?? '')
+                .toLowerCase()
+                .localeCompare((optionB?.label ?? '').toLowerCase())
+            }
+            options={users}
+            onChange={(e: any) => {
+              setTeacher(e)
+            }}
+          />
+        </div>
+      </Modal>
       <SectionMain>
         <CardBox className="mb-6" hasTable>
           <table>
@@ -134,13 +227,8 @@ const SemesterList = () => {
                 <th>Semester</th>
                 <th>Subjects</th>
                 <th>Created</th>
+                <th>Add Subject</th>
                 <th>Actions</th>
-                {/* <th>email</th>
-                <th>Address</th>
-                <th>Contact</th>
-                <th className="flex justify-center py-5">Role</th>
-              
-                <th>Actions</th> */}
               </tr>
             </thead>
             {/* {JSON.stringify(semesterPagination)} */}
@@ -193,7 +281,17 @@ const SemesterList = () => {
                     </div>
                   </td>
                   <td data-label="address">
-                    <span className="text-gray-700">{moment(semester.createdAt).fromNow()}</span>
+                    <div className="text-gray-700">{moment(semester.createdAt).fromNow()}</div>
+                  </td>
+                  <td className="before:hidden lg:w-1 whitespace-nowrap">
+                    <BaseButtons type="justify-start lg:justify-end" noWrap>
+                      <BaseButton
+                        color="success"
+                        icon={mdiPlus}
+                        onClick={() => showModal(semester.id)}
+                        small
+                      />
+                    </BaseButtons>
                   </td>
                   <td className="before:hidden lg:w-1 whitespace-nowrap">
                     <BaseButtons type="justify-start lg:justify-end" noWrap>
